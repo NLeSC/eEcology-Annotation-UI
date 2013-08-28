5fc99692a1b1e188069fd706181a7b7a3cfd4c65
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2013-08-28 09:58:50 +0200

    Use Google Auto-Loading.
    Make 'ant test' not crash, by including all required js files in karma.

--- karma.conf.js
+++ karma.conf.js
@@ -14,12 +14,16 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/lib/ExtSpec-*/ExtSpec.js',
+      // all js files used in index.html
       'resources/libs/d3/d3.js',
       'resources/libs/popcorn/popcorn-complete.min.js',
-      'https://www.google.com/jsapi',
+      'https://www.google.com/jsapi?autoload=%7B%22modules%22%3A%5B%7B%22version%22%3A%221%22%2C%22name%22%3A%22earth%22%7D%2C%7B%22version%22%3A%223.xx%22%2C%22other_params%22%3A%22sensor%3Dfalse%22%2C%22name%22%3A%22maps%22%7D%5D%7D',
+      // stubbed ExtJS
+      'test/lib/ExtSpec-*/ExtSpec.js',
+      // app
       'app/**/*.js',
-      'app.js',
+      'app/application.js',
+      // tests
       'test/specs/**/*Spec.js'
     ],
 
@@ -35,7 +39,6 @@ module.exports = function(config) {
     exclude: [
 
     ],
-
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
