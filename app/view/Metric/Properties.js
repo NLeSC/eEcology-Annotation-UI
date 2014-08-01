8c11d88a1a5f4d120f3aaf192e39342b279ff033
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2014-08-01 13:44:17 +0200

    Draw direction as arrow in track properties panel.

--- app/view/Metric/Properties.js
+++ app/view/Metric/Properties.js
@@ -7,18 +7,22 @@ Ext.define("TrackAnnot.view.Metric.Properties", {
     alias: 'widget.trackprops',
     autoScroll: true,
     tpl: [
-       'DT: {[values.date_time.toISOString()]}, Lon: {lon}, Lat: {lat}, ',
+       '<div>DT: {[values.date_time.toISOString()]}, Lon: {lon}, Lat: {lat}, ',
        'Alt: {altitude}, Temp: {temperature}, ',
        'Ground: {ground_elevation}, ',
        'Instantaneous speed: {speed}, Instantaneous direction: {direction}, ',
        'Delta instantaneous direction: {delta_direction}, ',
-       'Traject speed: {tspeed}, Traject direction: {tdirection}, ',
+       'Traject speed: {tspeed}, Traject direction: {tdirection}',
        'Acceleration: <tpl if="time_acceleration"><table>',
        '<tr><th>O</th><tpl for="time_acceleration"><td>{.}</td></tpl></tr>',
        '<tr><th>X</th><tpl for="x_acceleration"><td>{.}</td></tpl></tr>',
        '<tr><th>Y</th><tpl for="y_acceleration"><td>{.}</td></tpl></tr>',
        '<tr><th>Z</th><tpl for="z_acceleration"><td>{.}</td></tpl></tr>',
-       '<tpl else>NA</tpl>'
+       '</table><tpl else>NA</tpl></div>',
+       '<svg height="100" width="100">',
+       '<polygon points="50,5 40,95 60,95" style="fill:none;stroke:steelblue;" transform="rotate({direction} 50 50)"/>',
+       '<polygon points="50,5 40,95 60,95" style="fill:none;stroke:chocolate;" transform="rotate({tdirection} 50 50)"/>',
+       '</svg>'
     ],
     config: {
     	trackStore: 'Track'
